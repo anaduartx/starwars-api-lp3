5596a496ec9bc898533496ff15a86ed82125d9df
Ana <72501453+anaduartx@users.noreply.github.com>
2022-08-24 13:38:30 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicAPI-2408',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
